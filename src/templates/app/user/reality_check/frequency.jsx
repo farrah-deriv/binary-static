569bc769ce7bc4947561545a21c61d0510d43f00
Farrah Mae Ochoa <farrah@deriv.com>
2021-10-10 16:13:28 +0400

    fix lint error

--- src/templates/app/user/reality_check/frequency.jsx
+++ src/templates/app/user/reality_check/frequency.jsx
@@ -4,7 +4,7 @@ import Wrapper from './wrapper.jsx';
 const Frequency = () => (
     <React.Fragment>
         <Wrapper>
-            <p id='reality_check_note'></p>
+            <p id='reality_check_note' />
         </Wrapper>
     </React.Fragment>
 );
